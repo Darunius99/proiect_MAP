fb536f30e23e12eb86b081b7f190f60c99de35d3
Darunius99 <darius-dorin.popovici@student.upt.ro>
2025-01-14 23:56:54 +0200

    Actualizare cod pentru conversia unui număr natural în număr roman

---
+++ README.md
@@ -0,0 +1,20 @@
+# Proiect: Transformarea numărului zecial în sitemul de numeratie romane
+
+## Scopul proiectului
+Acest proiect are ca scop transformarea unui număr arabic (între 1 și 3999) într-un număr roman corespunzător.
+
+## Detalii de implementare
+Proiectul pe care l-am realizat este în limbajul de programare C. Codul sursă este compilat cu ajutorul GCC și poate fi rulat atât local cât și într-un container Docker.
+
+## Platforma software utilizată
+- Limbaj de programare: C
+- Compilator: GCC
+- Editor: Visual Studio Code
+- Docker pentru containerizare
+
+## Instrucțiuni de rulare
+
+### Local (fără Docker):
+1. Clonați repository-ul:
+   ```bash
+   git clone https://github.com/Darunius99/transf_roman.git
--- transf_roman.c
+++ transf_roman.c
@@ -1,42 +1,73 @@
 #include <stdio.h>
+#include <string.h>
 
-void convertToRoman(int number) {
-    struct RomanNumeral {
-        int value;
-        char *symbol;
-    };
-
-    struct RomanNumeral roman[] = {
-        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
-        {1, "I"}
-    };
-
-    int i = 0;
-    while (number > 0) {
-        while (number >= roman[i].value) {
-            printf("%s", roman[i].symbol);
-            number -= roman[i].value;
-        }
-        i++;
-    }
-}
+int valoareSimbol(char);
 
 int main() {
-    int number;
+	int numar;
 
-    printf("Enter a number (1-3999): ");
-    scanf("%d", &number);
+	printf("Introduceti un numar natural intre 1 si 3999: ");
+	scanf("%d", &numar);
 
-    if (number <= 0 || number > 3999) {
-        printf("Number must be between 1 and 3999.\n");
-        return 1;
-    }
+	if (numar < 1 || numar > 3999) {
+		printf("Numarul trebuie sa fie intre 1 si 3999.\n");
+		return 1;
+	}
+	printf("Numarul roman este: ");
+	while (numar >= 1000) {
+		printf("M");
+		numar -= 1000;
+	}
+	while (numar >= 900) {
+		printf("CM");
+		numar -= 900;
+	}
+	while (numar >= 500) {
+		printf("D");
+		numar -= 500;
+	}
+	while (numar >= 400) {
+		printf("CD");
+		numar -= 400;
+	}
+	while (numar >= 100) {
+		printf("C");
+		numar -= 100;
+	}
+	while (numar >= 90) {
+		printf("XC");
+		numar -= 90;
+	}
+	while (numar >= 50) {
+		printf("L");
+		numar -= 50;
+	}
+	while (numar >= 40) {
+		printf("XL");
+		numar -= 40;
+	}
+	while (numar >= 10) {
+		printf("X");
+		numar -= 10;
+	}
+	while (numar >= 9) {
+		printf("IX");
+		numar -= 9;
+	}
+	while (numar >= 5) {
+		printf("V");
+		numar -= 5;
+	}
+	while (numar >= 4) {
+		printf("IV");
+		numar -= 4;
+	}
+	while (numar >= 1) {
+		printf("I");
+		numar -= 1;
+	}
 
-    printf("Roman numeral: ");
-    convertToRoman(number);
-    printf("\n");
+	printf("\n");
 
-    return 0;
+	return 0;
 }
